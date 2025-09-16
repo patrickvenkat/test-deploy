1633b1f75f822b0c5e046ffe8bd4e36458c45758
patrickvenkat <51687587+patrickvenkat@users.noreply.github.com>
2025-09-16 19:46:06 +0000

    test

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import base64
+from openai import AzureOpenAI
+
+endpoint = "https://foundry-091525.openai.azure.com/openai/deployments/gpt-35-turbo/chat/completions?api-version=2025-01-01-preview"
+deployment = "gpt-35-turbo"
+subscription_key = "3OpzjBAZkAslPFdtdTPYGc2GKxWMtJDbxZ8Rt9FVKlQeN1bzkx6VJQQJ99BIACYeBjFXJ3w3AAAAACOG0f49"
+
+# Initialize Azure OpenAI client with key-based authentication
+client = AzureOpenAI(
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+    api_version="2025-01-01-preview",
+)
+
+
+# Prepare the chat prompt
+chat_prompt = [{"role": "user", "content": "What is the capital of France?"}]
+
+# Include speech result if speech is enabled
+messages = chat_prompt
+
+# Generate the completion
+completion = client.chat.completions.create(
+    model=deployment,
+    messages=messages,
+    max_tokens=1600,
+    temperature=0.7,
+    top_p=0.95,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=None,
+    stream=False
+)
+
+print(completion.to_json())    
